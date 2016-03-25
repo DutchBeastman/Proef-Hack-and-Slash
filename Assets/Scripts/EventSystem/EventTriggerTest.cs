1bba73c21932ab987279760b11c9e6adfa1db2a1
Jeremy Bond <jeremybond2@hotmail.com>
2016-03-25 16:56:46 +0100

    added an unity event system with test scene

---
+++ Assets/Scripts/EventSystem/EventTriggerTest.cs
@@ -0,0 +1,29 @@
+﻿//Created By: Jeremy Bond
+//Date: 25/03/2016
+
+using UnityEngine;
+using System.Collections;
+
+public class EventTriggerTest : MonoBehaviour
+{
+
+	protected void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.W))
+		{
+			EventManager.TriggerEvent ("Walk");
+		}
+		if (Input.GetKeyDown (KeyCode.S))
+		{
+			EventManager.TriggerEvent ("MoonWalk");
+		}
+		if (Input.GetKeyDown (KeyCode.A))
+		{
+			EventManager.TriggerEvent ("RotateLeft");
+		}
+		if (Input.GetKeyDown (KeyCode.D))
+		{
+			EventManager.TriggerEvent ("RotateRight");
+		}
+	}
+}
---
+++ Assets/Scripts/EventSystem/TriggerTest.cs
@@ -0,0 +1,63 @@
+﻿//Created By: Jeremy Bond
+//Date: 25/03/2016
+
+using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+using System;
+
+public class TriggerTest : MonoBehaviour
+{
+	private UnityAction someListener;
+	private const string EVENTW = "Walk";
+	private const string EVENTS = "MoonWalk";
+	private const string EVENTA = "RotateLeft";
+	private const string EVENTD = "RotateRight";
+
+	void Awake ()
+	{
+		someListener = new UnityAction (SomeFunction);
+	}
+
+	private void SomeFunction ()
+	{
+
+	}
+
+	void OnEnable ()
+	{
+		EventManager.StartListening (EVENTW, MoveForward);
+		EventManager.StartListening (EVENTS, MoveBackwards);
+		EventManager.StartListening (EVENTA, RotateLeft);
+		EventManager.StartListening (EVENTD, RotateRight);
+	}
+
+	void OnDisable ()
+	{
+		EventManager.StopListening (EVENTW, MoveForward);
+		EventManager.StopListening (EVENTS, MoveBackwards);
+		EventManager.StopListening (EVENTA, RotateLeft);
+		EventManager.StopListening (EVENTD, RotateRight);
+	}
+
+	private void MoveForward ()
+	{
+		Vector3 targetPosition = transform.position;
+		targetPosition.y += 1;
+		transform.position = Vector3.Lerp (transform.position, targetPosition, 0.5f);
+	}
+	private void MoveBackwards ()
+	{
+		Vector3 targetPosition = transform.position;
+		targetPosition.y -= 1;
+		transform.position = Vector3.Lerp (transform.position, targetPosition, 0.5f);
+	}
+	private void RotateRight ()
+	{
+		transform.Rotate (new Vector3 (0, 0, -10));
+	}
+	private void RotateLeft ()
+	{
+		transform.Rotate (new Vector3 (0, 0, 10));
+	}
+}

1924ebd5ccc98a0399b8536c72204d36c03ca80f
Fabian Verkuijlen <FabianVerk@hotmail.com>
2016-03-28 00:51:05 +0200

    Initializer changed

---
+++ Assets/Scripts/Initializer/Initializer.cs
@@ -0,0 +1,17 @@
+﻿//Created by: Fabian Verkuijlen
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Initializer : MonoBehaviour
+{
+    protected void Awake()
+    {
+        Invoke("TransitionToMain", 4f);
+    }
+    private void TransitionToMain()
+    {
+        SceneManager.LoadSceneAsync("Game");
+    }
+}

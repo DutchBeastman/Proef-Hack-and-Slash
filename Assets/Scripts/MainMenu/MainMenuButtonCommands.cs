9b3c71d8718f6e93c1d7f7f1a74efce6d024f469
Jeremy Bond <jeremybond2@hotmail.com>
2016-03-27 14:12:33 +0200

    adding main menu

---
+++ Assets/Scripts/MainMenu/MainMenuButtonCommands.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButtonCommands : MonoBehaviour
+{
+
+	public void Play ()
+	{
+
+		Debug.Log ("Log Start Game");
+	}
+
+	public void Settings ()
+	{
+
+		Debug.Log ("Log Settings Game");
+	}
+
+	public void Exit ()
+	{
+		Debug.Log ("Log Quit Game");
+		Application.Quit ();
+	}
+}

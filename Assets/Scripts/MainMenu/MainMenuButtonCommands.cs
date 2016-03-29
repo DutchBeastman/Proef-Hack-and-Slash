5140ba8ac95a628afd5174010cdc6db3b1e6ec86
Jeremy Bond <jeremybond2@hotmail.com>
2016-03-29 21:08:16 +0200

    adding settings and fading in and out to the main menu

--- Assets/Scripts/MainMenu/MainMenuButtonCommands.cs
+++ Assets/Scripts/MainMenu/MainMenuButtonCommands.cs
@@ -1,24 +1,70 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿//Created By: Jeremy Bond
+//Date: 29/03/2016
+
+using UnityEngine;
 
 public class MainMenuButtonCommands : MonoBehaviour
 {
-
-	public void Play ()
+	[SerializeField] private GameObject mainMenu;
+	[SerializeField] private GameObject settings;
+	
+	private bool fullAlpha;
+	
+	public void PlayButtonPushed ()
 	{
-
-		Debug.Log ("Log Start Game");
+		Overlay.FadeIn ();
+		Debug.Log ("Log Start");
 	}
 
-	public void Settings ()
+	public void SettingsButtonPushed ()
 	{
-
-		Debug.Log ("Log Settings Game");
+		Overlay.FadeIn ();
+		Invoke ("SwitchFromMainToSettings", 1);
 	}
 
-	public void Exit ()
+	public void ExitButtonPushed ()
 	{
-		Debug.Log ("Log Quit Game");
+		Debug.Log ("Log Quit");
 		Application.Quit ();
+	}
+
+	public void BackToMainMenuButtonPushed()
+	{
+		Overlay.FadeIn ();
+		Invoke ("SwitchFromSettingsToMain", 1);
+	}
+
+	private void SwitchFromMainToSettings ()
+	{
+		DisableMainMenu ();
+		EnableSettings ();
+		Overlay.FadeOut();
+	}
+
+	private void SwitchFromSettingsToMain ()
+	{
+		DisableSettings ();
+		EnableMainMenu ();
+		Overlay.FadeOut ();
+	}
+
+	private void EnableMainMenu ()
+	{
+		mainMenu.gameObject.SetActive (true);
+	}
+
+	private void DisableMainMenu ()
+	{
+		mainMenu.gameObject.SetActive (false);
+	}
+
+	private void EnableSettings ()
+	{
+		settings.gameObject.SetActive (true);
+	}
+
+	private void DisableSettings ()
+	{
+		settings.gameObject.SetActive (false);
 	}
 }
---
+++ Assets/Scripts/MainMenu/Settings.cs
@@ -0,0 +1,66 @@
+﻿//Created By: Jeremy Bond
+//Date: 
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class Settings : MonoBehaviour
+{
+	[SerializeField] private AudioMixer mixer;
+	[SerializeField] private Slider masterVolume;
+	[SerializeField] private Slider SFXVolume;
+	[SerializeField] private Slider musicVolume;
+
+	[SerializeField] private AudioClip volumeChangedSound;
+
+	private bool afterSoundPlayed = true;
+	private float lastMasterVolume;
+	private float lastSFXVolume;
+	private float lastMusicVolume;
+
+	protected void Update ()
+	{
+		if (lastMasterVolume != masterVolume.value)
+		{
+			ChangeMasterVolume();
+		}
+		else if(lastSFXVolume != SFXVolume.value)
+		{
+			ChangeSFXVolume();
+		}
+		else if (lastMusicVolume != musicVolume.value)
+		{
+			ChangeMusicVolume();
+		}
+		else if (!afterSoundPlayed)
+		{
+			afterSoundPlayed = true;
+			EventManager.TriggerAudioSFXEvent (volumeChangedSound);
+		}
+	}
+
+	private void ChangeMasterVolume ()
+	{
+		lastMasterVolume = masterVolume.value;
+		AudioListener.volume = masterVolume.value;
+		mixer.SetFloat ("MixerMasterVolume", masterVolume.value);
+		afterSoundPlayed = false;
+	}
+
+	private void ChangeSFXVolume ()
+	{
+		lastSFXVolume = SFXVolume.value;
+		AudioListener.volume = SFXVolume.value;
+		mixer.SetFloat ("MixerSFXVolume", SFXVolume.value);
+		afterSoundPlayed = false;
+	}
+
+	private void ChangeMusicVolume ()
+	{
+		lastMusicVolume = musicVolume.value;
+		AudioListener.volume = musicVolume.value;
+		mixer.SetFloat ("MixerMusicVolume", musicVolume.value);
+		afterSoundPlayed = false;
+	}
+}

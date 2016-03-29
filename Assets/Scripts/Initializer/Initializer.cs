a41e76298ad5dd282caf2f71be3d29167b85bc4e
Jeremy Bond <jeremybond2@hotmail.com>
2016-03-29 21:08:31 +0200

    adding fading in and out to the splash screen

--- Assets/Scripts/Initializer/Initializer.cs
+++ Assets/Scripts/Initializer/Initializer.cs
@@ -6,12 +6,20 @@ using UnityEngine.SceneManagement;
 
 public class Initializer : MonoBehaviour
 {
-    protected void Awake()
-    {
-        Invoke("TransitionToMain", 4f);
-    }
-    private void TransitionToMain()
-    {
-        SceneManager.LoadSceneAsync("Game");
-    }
+	protected void Awake()
+	{
+		Invoke ("FadingIn", 3f);
+		Invoke ("TransitionToMain", 4f);
+	}
+
+	private void FadingIn ()
+	{
+		Overlay.FadeIn();
+		print("start fading");
+	}
+
+	private void TransitionToMain()
+	{
+		SceneManager.LoadSceneAsync("Game");
+	}
 }
